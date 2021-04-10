cf4d1648b0bdb366245cb295aca7f1638c553d77
helenefav <helene.favrault@datadoghq.com>
2021-04-10 13:26:39 +0200

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,18 @@
+const config = {
+    s3: {
+      REGION: "us-west-2",
+      BUCKET: "notes-app-helene",
+    },
+    apiGateway: {
+      REGION: "us-west-2",
+      URL: "https://0w4yfguspe.execute-api.us-west-2.amazonaws.com/prod",
+    },
+    cognito: {
+      REGION: "us-west-2",
+      USER_POOL_ID: "us-west-2_GIAfgkgNi",
+      APP_CLIENT_ID: "19otv45ng6binjj4nv9qlksfgs",
+      IDENTITY_POOL_ID: "us-west-2:94e72e38-3cf6-4295-ba19-529cc7cfc55f",
+    },
+  };
+  
+  export default config;
