73db7b8f5528dab2379b3bfda915f3db50fb7a82
helenefav <helene.favrault@datadoghq.com>
2021-04-11 14:23:43 +0200

    Building our React app

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 const config = {
+  STRIPE_KEY: "YOUR_STRIPE_PUBLIC_KEY",
+    MAX_ATTACHMENT_SIZE: 5000000,
     s3: {
       REGION: "us-west-2",
       BUCKET: "notes-app-helene",
@@ -13,6 +15,8 @@ const config = {
       APP_CLIENT_ID: "19otv45ng6binjj4nv9qlksfgs",
       IDENTITY_POOL_ID: "us-west-2:94e72e38-3cf6-4295-ba19-529cc7cfc55f",
     },
+    
   };
+  
   
   export default config;
